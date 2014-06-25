6125edf811b3e575dd83003b315507fc01e86b59
zirm <zeerem@gmail.com>
2014-06-25 15:30:41 -0700

    Signed-off-by: zirm <zeerem@gmail.com>

---
+++ Hmwk/Hello_World_Homework/main.cpp
@@ -0,0 +1,23 @@
+/* 
+ * File:   Hello World
+ * Author: David Leanos
+ *Created on June 25, 2014, 2:51 PM
+ * Purpose: Homework Assigment
+ */
+
+#include <iostream>
+//system Level Libraries 
+using namespace std;
+// User Defined Libraries
+//Global Constants
+//Function Prototypes 
+
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+//Output a String
+    cout<<"Hello World"<<endl;
+    // Exit Stage Right
+    return 0;
+}
+
